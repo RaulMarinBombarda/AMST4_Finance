c9b7cdc4ea640967deaf1b1609722fe80609eb99
andrey.bertolettiteste@gmail.com <brendonymoura@gmail.com>
2024-09-16 13:47:58 -0300

    first

---
+++ Financia/Infraescture/Context/ApplicationDataContext.cs
@@ -0,0 +1,16 @@
+﻿using Financia.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Financia.iNFRAESCTURE.Context
+{
+    public class ApplicationDataContext: DbContext
+    {
+        
+    
+        public ApplicationDataContext(DbContextOptions<ApplicationDataContext> opitions) : base(opitions)
+        {
+
+        }
+        public DbSet<Category> Category { get; set; }
+    }
+}
---
+++ Financia/Models/Category.cs
@@ -0,0 +1,12 @@
+﻿namespace Financia.Models
+{
+    public class Category
+    {
+        public Guid Id { get; set; }
+        public string Description { get; set; }
+        public string Nome { get; set; }
+        public DateTime  CreatAt{ get; set; }
+        public bool IsActive { get; set; }
+
+    }
+}
