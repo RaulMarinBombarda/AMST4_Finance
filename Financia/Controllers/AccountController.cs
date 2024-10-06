b717b1c8ac3754516e516ad06ed4d4dd40bb32dd
RaulMarinBombarda <raulmarinescola@gmail.com>
2024-10-06 15:46:20 -0300

    registrodecontas

---
+++ Financia/Controllers/AccountController.cs
@@ -0,0 +1,168 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using Financia.Models;
+using Financia.iNFRAESCTURE.Context;
+
+namespace Financia.Controllers
+{
+    public class AccountController : Controller
+    {
+        private readonly ApplicationDataContext _context;
+
+        public AccountController(ApplicationDataContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Account
+        public async Task<IActionResult> Index()
+        {
+            return View(await _context.Account.ToListAsync());
+        }
+
+        // GET: Account/Details/5
+        public async Task<IActionResult> Details(Guid? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var account = await _context.Account
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (account == null)
+            {
+                return NotFound();
+            }
+
+            return View(account);
+        }
+
+        // GET: Account/Create
+        public IActionResult Create()
+        {
+            ViewBag.CategoryList = new SelectList(_context.Category, "Id", "Nome");
+
+            return View();
+        }
+
+        // POST: Account/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        //[ValidateAntiForgeryToken]
+        //public async Task<IActionResult> Create([Bind("Id,Nome,Description,Price,CreatedAt,DueDate,IsActive")] Account account)
+        //{
+        //    if (ModelState.IsValid)
+        //    {
+        //        account.Id = Guid.NewGuid();
+        //        _context.Add(account);
+        //        await _context.SaveChangesAsync();
+        //        return RedirectToAction(nameof(Index));
+        //    }
+        //    return View(account);
+        //}
+        public IActionResult Create(Account account)
+        {
+            account.Id = Guid.NewGuid();
+            _context.Add(account);
+            _context.SaveChanges();
+            return RedirectToAction("Index");
+
+        }
+
+        // GET: Account/Edit/5
+        public async Task<IActionResult> Edit(Guid? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var account = await _context.Account.FindAsync(id);
+            if (account == null)
+            {
+                return NotFound();
+            }
+            return View(account);
+        }
+
+        // POST: Account/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(Guid id, [Bind("Id,Nome,Description,Price,CreatedAt,DueDate,IsActive")] Account account)
+        {
+            if (id != account.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(account);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!AccountExists(account.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(account);
+        }
+
+        // GET: Account/Delete/5
+        public async Task<IActionResult> Delete(Guid? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var account = await _context.Account
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (account == null)
+            {
+                return NotFound();
+            }
+
+            return View(account);
+        }
+
+        // POST: Account/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(Guid id)
+        {
+            var account = await _context.Account.FindAsync(id);
+            if (account != null)
+            {
+                _context.Account.Remove(account);
+            }
+
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool AccountExists(Guid id)
+        {
+            return _context.Account.Any(e => e.Id == id);
+        }
+    }
+}
--- Financia/Models/Category.cs
+++ Financia/Models/Category.cs
@@ -1,12 +1,15 @@
-﻿namespace Financia.Models
+﻿using System.Net.Http.Headers;
+
+namespace Financia.Models
 {
     public class Category
     {
         public Guid Id { get; set; }
-        public string Description { get; set; }
-        public string Nome { get; set; }
-        public DateTime  CreatAt{ get; set; }
-        public bool IsActive { get; set; }
 
+        public string Nome { get; set; }
+        public string Description { get; set; }
+        public DateTime CreatAt{ get; set; }
+        public bool IsActive { get; set; }
+        public virtual List<Account> Accounts { get; set; }
     }
 }
